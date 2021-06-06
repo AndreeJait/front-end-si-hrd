118efed734c2b51cee5e72a68c866846ae3bd3a3
AndreJait <panjaitanandree@gmail.com>
2021-06-07 01:23:55 +0700

    make it better

--- src/containers/pages/Dashboard/index.jsx
+++ src/containers/pages/Dashboard/index.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Line } from 'react-chartjs-2'
 import './index.css'
-import axios from 'axios';
-import { APIBASE } from '../../../config/constants';
 class Dashboard extends Component {
   constructor(props) {
     super(props)
@@ -31,15 +28,6 @@ class Dashboard extends Component {
           <div className="side-right">
             <h5>Jabatan Fungsional</h5>
             <div className="row-jabatan">
-              {this.state.jabatan.length !== 0 && this.state.jabatan.map((item, index) => {
-                return (
-                  <div className="item-jabatan ">
-                    <h6>{item.positions[0].jabatan_fungsional}</h6>
-                    <strong>{item.private_data.nama}</strong><br />
-                    <small>{(new Date(item.positions[0].tmt)).toLocaleDateString("id-ID", {timeZone: "Asia/Jakarta", day:"2-digit", month:"long", year: "numeric"})}</small>
-                  </div>
-                )
-              })}
             </div>
           </div>
         </div>
